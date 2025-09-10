8b37ae641a0872e8458e0021e870d380fcc4ce4f
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-09-10 14:48:07 -0300

    Cria o hook useFetch

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-import Input from '../Forms/Input';
-import Button from '../Forms/Button';
-import useForm from '../../Hooks/useForm'
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import Error from "../Helper/Error";
+import useForm from "../../Hooks/useForm";
 import { USER_POST } from "../../api";
-import {UserContext} from '../../UserContext';
+import { UserContext } from "../../UserContext";
+import useFetch from "../../Hooks/useFetch";
 
 const LoginCreate = () => {
   const username = useForm();
-  const email = useForm('email');
+  const email = useForm("email");
   const password = useForm();
 
-  const {userLogin} = React.useContext(UserContext);
+  const { userLogin } = React.useContext(UserContext);
+
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const {url, options} = USER_POST({
+    const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
       password: password.value,
     });
 
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) {
       userLogin(username.value, password.value);
-    };
-  };
+    }
+  }
 
   return (
     <section className="animeLeft">
@@ -33,7 +37,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
