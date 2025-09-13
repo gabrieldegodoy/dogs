e009b393ff3b1cfc9276fe8b100b535f6de0a9ed
Gabriel de Godoy <gabriel.trabalhodg@gmail.com>
2025-09-13 17:08:23 -0300

    corrige bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -15,7 +15,7 @@ import NotFound from './Components/NotFound';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <UserStorage>
           <Header />
           <main className="AppBody">
@@ -37,7 +37,7 @@ function App() {
           </main>
           <Footer />
         </UserStorage>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
